a4c01fb939728670bd792ba691735724691250c6
Uribe Echenique <j.uribe.echenique@accenture.com>
2018-09-07 11:55:14 -0500

    login module was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
@@ -10,6 +11,12 @@ import { HotelsModule } from './features/hotels/hotels.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HotelsComponent } from './features/hotels/hotels.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: HotelsComponent },
+  { path: 'hotels', component: HotelsComponent }
+];
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -18,6 +25,9 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
     HotelsModule,
     HttpClientModule,
     NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: isDevMode() ? true : false
+    }),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     isDevMode()
